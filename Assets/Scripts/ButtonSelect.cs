a5b4064a9bc9bc67bc9b3c0905df61ea0dca3a97
zemiranda <zemiranda04@gmail.com>
2024-11-16 23:24:58 +0000

    selecao das personagens

---
+++ Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSelect : MonoBehaviour
+{
+
+    public int newValue; // Valor a ser atribuído
+
+    public void OnButtonPress()
+    {
+        if (PlayerScript.Instance != null)
+        {
+            // Tenta modificar p1 primeiro
+            if (PlayerScript.Instance.p1 == 0)
+            {
+                PlayerScript.Instance.SetPValue(1, newValue);
+                Debug.Log("p1 foi alterado.");
+            }
+            // Se p1 já foi escolhido, modifica p2
+            else if (PlayerScript.Instance.p2 == 0)
+            {
+                PlayerScript.Instance.SetPValue(2, newValue);
+                Debug.Log("p2 foi alterado.");
+            }
+            else
+            {
+                Debug.LogWarning("Ambas as variáveis (p1 e p2) já foram definidas!" + PlayerScript.Instance.p1 + PlayerScript.Instance.p2);
+            }
+        }
+        else
+        {
+            Debug.LogError("PlayerScript não foi encontrado!");
+        }
+    }
+}
--- Assets/Scripts/Player Script.cs
+++ Assets/Scripts/Player Script.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PlayerScript Instance; // Singleton
+
+    public int p1 = 0; // Valor inicial como 0 (não definido)
+    public int p2 = 0; // Valor inicial como 0 (não definido)
+
+    private void Awake()
     {
-        
+        // Configurar Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Método para definir os valores
+    public void SetPValue(int pNumber, int value)
     {
-        
+        if (pNumber == 1)
+        {
+            p1 = value;
+            Debug.Log($"p1 foi definido para {value}");
+        }
+        else if (pNumber == 2)
+        {
+            p2 = value;
+            Debug.Log($"p2 foi definido para {value}");
+        }
     }
 }
+
