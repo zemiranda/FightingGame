d0dbe2610aafdf638710f1db2c8364af87a21776
zemiranda <zemiranda04@gmail.com>
2024-11-17 01:01:11 +0000

    Deselect player and start fight scene

--- Assets/Scripts/Player Script.cs
+++ Assets/Scripts/Player Script.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -8,6 +9,41 @@ public class PlayerScript : MonoBehaviour
 
     public int p1 = 0; // Valor inicial como 0 (não definido)
     public int p2 = 0; // Valor inicial como 0 (não definido)
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PlayerScript.Instance != null)
+            {
+                PlayerScript.Instance.DeselectValues();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryChangeScene();
+        }
+    }
+
+    public void DeselectValues()
+    {
+
+        if (p2 != 0) // Deselect p2 first if it is selected
+        {
+            Debug.Log($"p2 ({p2}) foi deselecionado.");
+            p2 = 0;
+        }
+        else if (p1 != 0) // Then deselect p1
+        {
+            Debug.Log($"p1 ({p1}) foi deselecionado.");
+            p1 = 0;
+        }
+        else
+        {
+            Debug.Log("Nenhuma variável está selecionada.");
+        }
+    }
 
     private void Awake()
     {
@@ -34,6 +70,20 @@ public class PlayerScript : MonoBehaviour
         {
             p2 = value;
             Debug.Log($"p2 foi definido para {value}");
+        }
+    }
+
+    // Método para trocar de cena se ambos os jogadores estiverem selecionados
+    private void TryChangeScene()
+    {
+        if (p1 != 0 && p2 != 0)
+        {
+            Debug.Log("Ambos os jogadores estão selecionados. Trocando de cena...");
+            SceneManager.LoadScene("Fight Scene"); // Substitua pelo nome ou índice da cena
+        }
+        else
+        {
+            Debug.LogWarning("Não é possível trocar de cena. Ambos os jogadores precisam estar selecionados.");
         }
     }
 }
